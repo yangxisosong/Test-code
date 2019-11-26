c200ea395d86cf6f8c106d6ed2f86e48943d55ee
yangxisosong <45442989+yangxisosong@users.noreply.github.com>
2019-11-26 21:29:15 +0800

    测试

---
+++  first.js
@@ -0,0 +1,11 @@
+var FirstClass = /** @class */ (function () {
+    function FirstClass() {
+        this.name = "class";
+        this.age = 12;
+    }
+    FirstClass.prototype.output = function () {
+        console.log("name is" + this.name);
+    };
+    return FirstClass;
+}());
+//# sourceMappingURL= first.js.map
---
+++  first.ts
@@ -0,0 +1,13 @@
+class FirstClass {
+
+    name: string;
+    age: number;
+    constructor() {
+    
+        this.name = "class";
+        this.age = 12;
+    }
+    output() {
+        console.log("name is" + this.name);
+    }
+}
